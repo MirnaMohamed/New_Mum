0b6ecfe5211aa631b9792b49eaa752782235d112
MirnaMohamed <mirnaattallah00@gmail.com>
2023-09-01 15:33:24 +0300

    initial commit

---
+++ src/main/java/com/example/new_mum/NewMumApplication.java
@@ -0,0 +1,13 @@
+package com.example.new_mum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewMumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/new_mum/NewMumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.new_mum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewMumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
